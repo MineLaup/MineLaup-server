47effec4685ee453e80d8b9767ce70683b79e851
Louis Bailleau <louis.bailleau@free.fr>
2020-07-25 15:56:13 +0200

    Created get modpack route

--- backend/app/Controllers/Http/ModpacksController.ts
+++ backend/app/Controllers/Http/ModpacksController.ts
@@ -2,6 +2,9 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import Modpack from 'App/Models/Modpack'
+import Team from 'App/Models/Team'
+import TeamUser from 'App/Models/TeamUser'
+import TeamRole from 'App/Models/TeamRole'
 
 export default class ModpacksController {
   /**
@@ -19,6 +22,62 @@ export default class ModpacksController {
       .firstOrFail()
 
     return response.json(user.teams)
+  }
+
+  /**
+   * Get a modpack
+   */
+  public async get ({ auth, response, request}: HttpContextContract) {
+    const data = await request.validate({
+      schema: schema.create({
+        id: schema.number([
+          rules.unsigned(),
+          rules.exists({
+            column: 'id',
+            table: 'modpacks',
+          }),
+        ]),
+      }),
+    })
+
+    // Get the modpack
+    const modpack = await Modpack.findOrFail(data.id)
+
+    // Check if the user has the right to get the modpack
+    const teamUser = await TeamUser
+      .query()
+      .preload('teamRole', (query: TeamRole) => {
+        query.preload('permission')
+      })
+      .preload('team', (query: Team) => {
+        query.preload('defaultPermission')
+      })
+      .where('user_id', auth.user!.id)
+      .andWhere('team_id', modpack.teamId)
+      .firstOrFail()
+
+    let userPerms: any = null
+
+    // Is the user is the owner
+    if (teamUser.team.ownerId === auth.user!.id) {
+      // Set the permissions
+      userPerms = {
+        owner: true,
+      }
+    // Is the user have a role
+    } else if (teamUser.teamRoleId) {
+      // Set the permissions
+      userPerms = teamUser.teamRole.permission
+    } else {
+      userPerms = teamUser.team.defaultPermission
+    }
+
+    // Send the team information and permissions
+    response.send({
+      ...modpack.toJSON(),
+      team: teamUser.team.toJSON(),
+      userPerms,
+    })
   }
 
   /**
